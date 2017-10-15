7a4f277a7427949439eaa39df6f560b7d31cb713
root <root@ld03084.homedepot.com>
2017-10-15 09:46:57 -0400

    Added commands

---
+++ exec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Verify if Yum is pre-installed"
+yum --version
+
+yum install epel-release
+
+yum install nginx
+
+echo "When installation is complete - verify"
+
+systemctl start nginx
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service-https
+
+firewall-cmd reload
+systemctl enable nginx
+
+echo 'verifying if Nginx is up and running'
+
+curl 'http://172.24.101.82'
